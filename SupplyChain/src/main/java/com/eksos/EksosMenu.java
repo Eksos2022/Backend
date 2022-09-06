4d0cef3338c34c126c212691830f738ef49bf924
luis <ljrp30@gmail.com>
2022-09-05 22:15:02 -0600

    vista inventario

--- SupplyChain/src/main/java/com/eksos/EksosMenu.java
+++ SupplyChain/src/main/java/com/eksos/EksosMenu.java
@@ -4,6 +4,7 @@
  */
 package com.eksos;
 
+import com.eksos.views.InventoryView;
 import com.eksos.views.SuppliersView;
 
 /**
@@ -31,6 +32,7 @@ public class EksosMenu extends javax.swing.JFrame {
 
         jLabel1 = new javax.swing.JLabel();
         ButtonProveedores = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -46,6 +48,14 @@ public class EksosMenu extends javax.swing.JFrame {
         });
         getContentPane().add(ButtonProveedores, new org.netbeans.lib.awtextra.AbsoluteConstraints(341, 168, -1, -1));
 
+        jButton1.setText("Inventario");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(680, 160, -1, -1));
+
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
@@ -55,8 +65,15 @@ public class EksosMenu extends javax.swing.JFrame {
         this.setVisible(false);
     }//GEN-LAST:event_ButtonProveedoresActionPerformed
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        InventoryView frame = new InventoryView();
+        frame.setVisible(true);
+        this.setVisible(false);
+    }//GEN-LAST:event_jButton1ActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton ButtonProveedores;
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
---
+++ SupplyChain/src/main/java/com/eksos/controllers/InventoryController.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.eksos.controllers;
+
+/**
+ *
+ * @author ljrp3
+ */
+public class InventoryController {
+    
+}
